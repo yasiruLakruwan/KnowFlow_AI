0661d0a9ff359436ef3af85bee735e803d2017e9
yasiruLakruwan <yapa1459@gmail.com>
2025-12-01 19:53:27 +0530

    Developing modules

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,2 +1,10 @@
 from utils.embeding_model import *
 from config.path_config import *
+from logger import get_logger
+
+logger = get_logger(__name__)
+
+class DataIngestion:
+    def __init__(self,data_path):
+        self.data_path = data_path
+        
