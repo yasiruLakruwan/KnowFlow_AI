cc8bd3d7baaabaa5ef31baae99dad60c186f4ac3
yasiruLakruwan <yapa1459@gmail.com>
2025-12-09 01:55:56 +0530

    testing ingestion pipeline

--- src/ingestion/pipeline.py
+++ src/ingestion/pipeline.py
@@ -5,4 +5,22 @@ from config.path_config import *
 
 class main:
     def pipeline(self):
-        pass
+        # Directry loader
+        loader = DirectryLoader(data_dir)
+
+        documents = loader.load()
+
+        # Text chunking
+        for item in enumerate(documents):
+           file_name =  item['filename']
+           conent = item['content']
+           chunker = TextChunking(700,70)
+        
+           sections = chunker.split_into_sections(conent)
+           
+           for section in sections:
+               chunks = chunker.chunk_section(section)
+
+               for chunk in chunks:
+                  all_chunks = chunker.chunk(chunk,file_name)
+
