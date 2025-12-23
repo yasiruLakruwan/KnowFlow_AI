abd3440fae48dda93c93e4ea7f8ca0425fef0591
yasiruLakruwan <yapa1459@gmail.com>
2025-12-23 20:06:18 +0530

    creating llmclient

---
+++ src/llm/llm_client.py
@@ -0,0 +1,12 @@
+class llm_client:
+    def __init__(self,llm):
+        self.llm = llm
+
+    def genarate(self,prompt:str) -> str:
+        response = self.llm.invoke(prompt)
+
+        # Normalize response
+        if hasattr(response,"content"):
+            return response.content.strip()
+        
+        return str(response).strip()
