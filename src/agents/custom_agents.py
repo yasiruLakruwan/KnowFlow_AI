0884453eaef4fb78762c83cdeb93e9c12137ee63
yasiruLakruwan <yapa1459@gmail.com>
2025-12-30 15:08:45 +0530

    developing agents

--- src/agents/custom_agents.py
+++ src/agents/custom_agents.py
@@ -1,5 +1,23 @@
 class CustomAgents:
     def __init__(self,llm):
         self.llm = llm
-    def plan_aciton(self,llm,quesiton,chat_history):
+    def plan_aciton(self,quesiton,chat_history):
+        prompt = f"""
+    You are a RAG planner.
+
+    Conversation history:
+    {chat_history}
+    User question:
+    {quesiton}
+    Deside the next action:
+    - rewrite
+    - rewrite and retrive
+    - ask clarification
+    - asswer directly
+
+    Return ONLY one action
+    """
+        return self.llm.invoke(prompt).content.strip().lower()
+    
+    def critic(self,question,context,answer):
         pass
