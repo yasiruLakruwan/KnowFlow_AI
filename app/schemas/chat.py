77c6d15fee701b607e1945f5fddf886d089f50a0
yasiruLakruwan <yapa1459@gmail.com>
2026-01-07 21:20:47 +0530

    added pydantic class to the ragas

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -5,11 +5,16 @@ class ChatRequest(BaseModel):
     query:str
     session_id: Optional[str] = None
 
+class RagasMatrics(BaseModel):
+    context_precision: float
+    faithfulness: float
+    answer_relevancy: float
+
 class ChatResponse(BaseModel):
     session_id:str
     answer: str
     rewritten_query:str
     contexts: List[str]
-    ragas:Dict
+    ragas:RagasMatrics
 
     
