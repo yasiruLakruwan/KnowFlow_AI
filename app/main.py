b275a6270eb42d4dd4a8b85d557fe19068fba610
yasiruLakruwan <yapa1459@gmail.com>
2026-01-10 01:51:20 +0530

    added middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.api.chat import router as chat_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Know-flow AI RAG Backend",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chat_router,prefix="/api")
